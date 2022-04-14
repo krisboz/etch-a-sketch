af792ba6a19444ebe60269f80ccfd195d915abec
krisboz <kristijan.bozinovic1@gmail.com>
2022-04-14 19:47:04 +0200

    Added reset button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//Drawing the grid
 const container = document.querySelector(".container");
 let rows = document.getElementsByClassName("gridRow");
 
@@ -30,3 +30,39 @@ function makeGrid(rows, cols) {
 }
 
 makeGrid(16, 16)
+
+const fillSquare = (event) => {
+    event.target.classList.add("clicked")
+}
+
+const deleteGrid = () => {
+    const container = document.querySelector(".container")
+    while(container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+}
+
+const testFunc = () => {
+    deleteGrid();
+    let value = prompt("How big you wanna")
+    makeGrid(value, value);
+    const cells = document.querySelectorAll(".cell")
+
+    cells.forEach(cell => cell.addEventListener("mouseenter", fillSquare), {once: true})
+
+}
+
+//. The clear button function iterates through each grid box and 
+//reset the background color value to null. The reset button
+// function prompts the user to enter a number. If the number is 
+//valid and below 100, it will remove the old grid boxes and add
+// news ones based on the new number using the createGrid function.
+// Otherwise, it will re-prompt the user to enter a number if its
+// over 100 or do nothing.
+
+//Adding event listeners
+const cells = document.querySelectorAll(".cell")
+cells.forEach(cell => cell.addEventListener("mouseenter", fillSquare), {once: true})
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", testFunc)
